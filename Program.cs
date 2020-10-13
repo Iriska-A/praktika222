a61b2ddb2339a06c95516507b3781a1594697af5
Iriska-A <72428513+Iriska-A@users.noreply.github.com>
2020-10-13 19:35:49 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ClassLibrary3;
+
+namespace magazin
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Mag magg = new Mag("Введите имя", "Введите фамилию", "Введите Email", "Придумайте пароль", "Зарегистрироваться");
+
+            Console.WriteLine(magg.ReturnSurnameMag());
+
+
+            Console.WriteLine(magg.ReturnNameMag());
+            Console.WriteLine(magg.ReturnPasswordMag());
+            Console.WriteLine(magg.ReturnEmailMag());
+            Console.WriteLine(magg.ReturnInputMag());
+
+
+            Console.ReadLine();
+        }
+
+        class Library3
+        {
+            public static void Main(string[] args)
+
+            {
+                Cl1 can = new Cl1();
+
+                can.Hello("Ирина");
+
+                Console.WriteLine("Как настроение?");
+                Console.WriteLine("Через три года вам будет-(can.AgeToThree(18)");
+                Console.WriteLine();
+                Console.ReadLine();
+
+            }
+        }
+
+    }
+}
+
---
+++ mag.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace magazin
+{
+    class Mag
+    {
+        string surname;
+        string name;
+        string email;
+        string password;
+        string input;
+
+        public Mag(string surname, string name, string email, string password, string input)
+        {
+            this.surname = surname;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.input = input;
+        }
+        public string ReturnNameMag()
+        {
+            return name;
+        }
+        public string ReturnSurnameMag()
+        {
+            return surname;
+
+        }
+        public string ReturnEmailMag()
+        {
+            return email;
+        }
+
+        public string ReturnPasswordMag()
+        {
+            return password;
+        }
+
+        public string ReturnInputMag()
+        {
+            return input;
+        }
+        
+         
+          
+        void CreateMag()
+        {
+
+        }
+            
+    }
+}
